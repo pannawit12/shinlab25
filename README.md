8c4a80359fa33343de2dec57577fd8333a2df4f0
Pannawit Setsiriwanit <pannawit_se@cmu.ac.th>
2023-03-10 17:22:35 +0700

    3

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME:
-# STUDENT ID:
+# NAME:Pannawit Setsiriwanit
+# STUDENT ID:650610784
--- lab25_3.cpp
+++ lab25_3.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 //Write function count() here.
-
+#include<set>
+int count(int *data,int size){
+    set<int> myset;
+    for(int i=0;i<size;i++){
+        myset.insert(data[i]);
+    }
+    return myset.size();
+}
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
 	
